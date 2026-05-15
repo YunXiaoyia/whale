a4e59133734fc3640cc1feecc22b7a8714d8a1a5
yunyi <2457980080@qq.com>
2026-05-16 00:04:07 +0800

    Revert "添加 Agent Harness V1 API 和 CLI"
    This reverts commit ab2bc8ebacd48031de40dfdf48007cd11fd3626c.

--- whale/__init__.py
+++ whale/__init__.py
@@ -10,15 +10,12 @@ from .cli import (
     split_repl_command,
 )
 from .models import AnthropicCompatibleModelClient, FakeModelClient, OllamaModelClient, OpenAICompatibleModelClient
-from .harness import EvaluationHarness, HarnessRunResult, RuntimeHarness
 from .runtime import WhaleAgent, Whale, SessionStore
 from .workspace import WorkspaceContext
 
 __all__ = [
     "AnthropicCompatibleModelClient",
-    "EvaluationHarness",
     "FakeModelClient",
-    "HarnessRunResult",
     "Whale",
     "build_agent",
     "build_arg_parser",
@@ -28,7 +25,6 @@ __all__ = [
     "main",
     "slash_command_completer",
     "split_repl_command",
-    "RuntimeHarness",
     "WhaleAgent",
     "OllamaModelClient",
     "OpenAICompatibleModelClient",
